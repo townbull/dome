3e9e47d53840acd126458deae3c69634a180de9a
Bo Tang <botang.usa@gmail.com>
2014-01-12 14:53:43 -0600

    Changed database to sqlite3 for development

--- skymesh_dashboard/settings.py
+++ skymesh_dashboard/settings.py
@@ -68,11 +68,11 @@ WSGI_APPLICATION = 'skymesh_dashboard.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'test',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': os.path.join(BASE_DIR, 'dome.sqlite3'),                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'test',
-        'PASSWORD': 'test',
+        'USER': '',
+        'PASSWORD': '',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
