021ac2079f749e74d73ce754f9809bd84207b666
Bo Tang <botang.usa@gmail.com>
2014-01-20 21:41:52 -0600

    Changed static and template file location to root dir
    Moved static and template folders to the root directory so that all the
    apps may share the files equally.

--- skymesh_dashboard/settings.py
+++ skymesh_dashboard/settings.py
@@ -99,15 +99,17 @@ USE_TZ = True
 
 MEDIA_ROOT = os.path.abspath(os.path.join(BASE_DIR, 'media'))
 MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, 'dome/static'))
+# STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, 'static'))
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(
-        os.path.dirname(__file__),
-        'static',
-    ),
+    # os.path.join(
+    #     os.path.dirname(__file__),
+    #     'static',
+    # ),
+
+    os.path.join(BASE_DIR, "static"),
 )
--- skymesh_dashboard/urls.py
+++ skymesh_dashboard/urls.py
@@ -1,7 +1,4 @@
 from django.conf.urls import patterns, include, url
-from django.conf.urls.static import static
-from django.conf import settings
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 from django.contrib import admin
@@ -15,16 +12,3 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('dome.urls')),
 )
-
-# Development static app and project media serving using the staticfiles app.
-urlpatterns += staticfiles_urlpatterns()
-
-# Convenience function for serving user-uploaded media during
-# development. Only active if DEBUG==True and the URL prefix is a local
-# path. Production media should NOT be served by Django.
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-if settings.DEBUG:
-    urlpatterns += patterns('',
-        url(r'^500/$', 'django.views.defaults.server_error')
-    )
