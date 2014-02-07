d8f20037f1d2148059373d67ffbda38b3c57c5e7
fzb14 <fzb14@hotmail.com>
2014-02-07 00:06:32 -0600

    save file info into skymesh and show all items in skymesh
    use Chooser to add file to skymesh from Dropbox and show all items in
    skymesh; set up sqlite database; build FileInfo model;  add csrf
    solution for ajax post;

--- kiwi/urls.py
+++ kiwi/urls.py
@@ -5,5 +5,7 @@ import views
 urlpatterns = patterns('',
     url(r'^$', views.Selector, name='selector'),
     url(r'^dropbox/$', views.DropboxChooser, name='dropbox_chooser'),
-    url(r'^google/$', views.GooglePicker, name='google_picker')
+    url(r'^google/$', views.GooglePicker, name='google_picker'),
+    url(r'^dropbox/save$', views.SaveInfoDropbox, name='saveInfo_dropbox'),
+    url(r'^showlist$', views.Showlist, name='showlist')
 )
--- kiwi/views.py
+++ kiwi/views.py
@@ -1,11 +1,43 @@
 from django.shortcuts import render, render_to_response, redirect
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core import serializers
+from models import FileInfo
+import json
 # Create your views here.
 
 def Selector(request):
     return render_to_response('kiwi/selector.html')
 
+
 def GooglePicker(request):
     return
 
+
 def DropboxChooser(request):
     return
+
+
+#@csrf_exempt
+def SaveInfoDropbox(request):
+    # save information of dropbox item in db, return all existing items
+    print 'jquery post received'
+    print request.POST.get('files')
+    files = json.loads(request.POST.get('files'))
+
+    for f in files:
+        #print f["link"], f["bytes"], f["link"], f["icon"]
+        file = FileInfo(name=f["name"], link=f["link"], size=int(f["bytes"]), icon=f["icon"])
+        # icon=f["icon"], thumbnail=f["thumbnail"])
+        print 'file set done'
+        file.save()
+        print 'save success'
+
+    return render_to_response('kiwi/selector.html')
+        #json.loads(FileInfo.objects.all())
+
+def Showlist(request):
+    files = FileInfo.objects.all()
+    data = serializers.serialize('json', files)
+    data = '{"files":'+ data + '}'
+    return HttpResponse(data, content_type='application/json')
